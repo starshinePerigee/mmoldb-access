1df6e829566e1b15e071803ffc21a60f61d42e7e
starshinePerigee <skywing.ki@gmail.com>
2025-06-29 19:04:42 -0700

    Basic unit test and project configuration

---
+++ tests/test_crunch/test_access_config.py
@@ -0,0 +1,8 @@
+import pytest
+
+from crunch.access_config import GlobalConfig
+
+def test_globalconfig_init():
+    cfg = GlobalConfig()
+    assert isinstance(cfg["connection"]["host"], str)
+    assert isinstance(cfg["connection"]["port"], int)
